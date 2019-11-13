c529cbf127671d3278a90f71dfadc61f579550b8
Scott Etter <scoett@gadgetastrophe.com>
2019-11-13 12:33:15 -0800

    Updates

---
+++ precache-manifest.81dbaa88a994d9e018bf12425282712a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c8b3ccf8c62ac2d18f21dca415d949",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c37175216aea6f7c5086",
+    "url": "/static/css/main.c515700a.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "c37175216aea6f7c5086",
+    "url": "/static/js/main.aa6183a5.chunk.js"
+  },
+  {
+    "revision": "d6023e9a5195b54d38ce",
+    "url": "/static/js/runtime-main.e5eb06e0.js"
+  }
+]);
